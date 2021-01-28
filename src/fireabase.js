ac77948e78b89351b716fd84f5ad91391ace5aac
Robzlegz <robzlegz@gmail.com>
2021-01-28 09:11:39 +0200

    setting up firebase

---
+++ src/fireabase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCCUfFWkPo5HY9Wfh3ztwgfihBzuyj_-m0",
+    authDomain: "clone-f1a11.firebaseapp.com",
+    projectId: "clone-f1a11",
+    storageBucket: "clone-f1a11.appspot.com",
+    messagingSenderId: "682964477394",
+    appId: "1:682964477394:web:b8ff24b19e902b619d940b",
+    measurementId: "G-KCMB0BBLWZ"
+};
