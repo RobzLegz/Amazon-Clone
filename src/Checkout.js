bec515bf4e113a93e7dfb5c0603cb4bd28fa95e8
Robzlegz <robzlegz@gmail.com>
2021-02-01 21:31:51 +0200

    updating checkout page

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,13 +6,14 @@ import Subtotal from "./Subtotal";
 
 const Checkout = () => {
 
-    const [{basket}, dispatch] = useStateValue();
+    const [{basket, user}, dispatch] = useStateValue();
 
     return (
         <div className="checkout-container">
             <div className="checkout-left">
                 <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="ad" className="checkout-ad"/>
                 <div>
+                    <h3>{user?.email}</h3>
                     <h2 className="checkout-title">Your Shopping Basket</h2>
                 </div>
                 {basket.map(item => (
