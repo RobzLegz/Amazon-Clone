3afda383740ca8b24278bdc03a68c368b5058aeb
Robzlegz <robzlegz@gmail.com>
2021-02-01 20:28:45 +0200

    updating login page

--- src/Header.js
+++ src/Header.js
@@ -19,10 +19,12 @@ const Header = () => {
                 <SearchIcon className="header-search-icon" />
             </div>
             <div className="header-nav">
-                <div className="header-option">
-                    <span className="option-lineone">Hello Guest</span>
-                    <span className="option-linetwo">Sign In</span>
-                </div>
+                <Link to="/login">
+                    <div className="header-option">
+                        <span className="option-lineone">Hello Guest</span>
+                        <span className="option-linetwo">Sign In</span>
+                    </div>
+                </Link>
                 <div className="header-option">
                     <span className="option-lineone">Returns</span>
                     <span className="option-linetwo">&Orders</span>
---
+++ src/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "./Login.css";
+
+const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signIn = e => {
+        e.preventDefault();
+    }
+
+    const register = e => {
+        e.preventDefault();
+    }
+
+    return (
+        <div className="login">
+            <Link to="/">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg" className="login-logo" alt="login logo"/>
+            </Link>
+            <div className="login-container">
+                <h1>Sign In</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="email" value={email} onChange={e => setEmail(e.target.value)} required/>
+                    <h5>Password</h5>
+                    <input type="password" onChange={e => setPassword(e.target.value)} value={password} required/>
+                    <button type="submit" className="signin-btn">Sign In</button>
+                </form>
+                <p>By continuing, you agree to FAKE AMAZON'S Conditions of Use and Privacy Notice.</p>
+                <button onClick={register} className="register-btn" onClick={signIn}>Create your Amazon Account</button>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
