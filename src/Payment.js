b0843608ed6993195905ce17d06cfe28036bfc60
Robzlegz <robzlegz@gmail.com>
2021-02-12 21:26:12 +0200

    setting up axios

--- src/Payment.js
+++ src/Payment.js
@@ -1,4 +1,5 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import CurrencyFormat from 'react-currency-format';
 import { Link } from 'react-router-dom';
